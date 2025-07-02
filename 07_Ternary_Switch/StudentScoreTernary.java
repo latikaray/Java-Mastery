cae7a460d487a93eba99e9d3a9037a5e0126cf55
Latika Manoj Ray <latikar109@gmail.com>
2025-07-02 15:02:10 +0530

    Add files via upload

---
+++ 07_Ternary_Switch/StudentScoreTernary.java
@@ -0,0 +1,21 @@
+//Challenge 56 : Student Score using Ternary Operator
+import java.util.Scanner;
+
+class StudentScoreTernary {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to Student Score\n");
+        System.out.print("Please enter your marks: ");
+        int marks = input.nextInt();
+
+        String category = marks > 80 ? "High" : (marks > 50 ? "Moderate" : "Low");
+        System.out.println("Your category is: " + category);
+
+        input.close();
+    }
+}
+
+
+
+
+
