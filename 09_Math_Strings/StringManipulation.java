1d770559a42493776b6d9116abd493a11c9ba455
Latika Manoj Ray <latikar109@gmail.com>
2025-07-19 11:09:09 +0530

    Delete 09_Math_Strings directory

--- 09_Math_Strings/StringManipulation.java
+++
@@ -1,9 +0,0 @@
-//Challenge 71 : String Manipulation 
-class StringManipulation {
-    public static void main(String[] args) {
-        String firstName = "Latika";
-        String lasName = "Ray";
-        String fullName = firstName.concat( " ").concat(lasName);
-        System.out.println(fullName.toUpperCase());
-    }
-}
