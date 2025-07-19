e7cf8fa382b267b0f1b1866ba22d9f06ec3428e3
Latika Manoj Ray <latikar109@gmail.com>
2025-07-19 11:09:22 +0530

    Delete 10_Encapsulation directory

--- 10_Encapsulation/Company/EmployeeTest.java
+++
@@ -1,9 +0,0 @@
-package Company;
-
-public class EmployeeTest {
-    public static void main(String[] args) {
-        Employee emp = new Employee("Latika", 24, 50000);
-        emp.displayEmployeeDetails(); // Accessible within the same package
-    }
-}
-
