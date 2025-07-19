764424d9db7c01c4258f5f54ed8d7c35997f52fe
Latika Manoj Ray <latikar109@gmail.com>
2025-07-19 11:07:19 +0530

    Delete 01_Basics directory

--- 01_Basics/Morning.java
+++
@@ -1,7 +0,0 @@
-//Challenge 1 : Print "Good Morning" to the console
-
-class Morning {
-  public static void main(String[] args) {
-    System.out.print("Good Morning");
-  }
-}
--- 01_Basics/Subscribe.java
+++
@@ -1,7 +0,0 @@
-//Challenge 2 : Print "Subscribe" to the console
-
-class Subscribe {
-    public static void main(String[] args) {
-        System.out.println("Subscribe");
-    }
-}
