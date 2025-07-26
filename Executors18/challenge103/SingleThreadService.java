12016bddc669b96edeef446c978cae2f62cdb938
Latika Manoj Ray <latikar109@gmail.com>
2025-07-26 11:04:14 +0530

    Challenges 103 - 105 added

---
+++ Executors18/challenge103/SingleThreadService.java
@@ -0,0 +1,12 @@
+//Challenge 103 : Single Thread Executor Service
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class SingleThreadService {
+    public static void main(String[] args) {
+        try (ExecutorService service = Executors.newSingleThreadExecutor()) {
+          PrintNumbers task = new PrintNumbers();
+          service.submit(task);
+        }
+    }
+}
